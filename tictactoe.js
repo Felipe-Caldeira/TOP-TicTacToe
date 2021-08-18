14a2ccdd84dafa77d39e5275c4fbdbefa6c6d8eb
Felipe Caldeira <hms1730105@gmail.com>
2021-08-18 02:50:25 -0500

    Added HTML, CSS, and fixed JS

--- tictactoe.js
+++ tictactoe.js
@@ -1,10 +1,13 @@
-// Gameboard object
+// Gameboard module
 var gameboard = (function() {
     'use strict';
 
     var _gbArray = [0, 0, 0,
-                    0, 0, 0,
-                    0, 0, 0];
+        0, 0, 0,
+        0, 0, 0
+    ];
+
+    var _freeSpaces = 9;
 
     var _winningIndeces = [
         [0, 1, 2],
@@ -17,104 +20,146 @@ var gameboard = (function() {
         [2, 4, 6]
     ];
 
+    var _cells = document.querySelectorAll(".cell");
+
+
     function checkWinCondition() {
-        for (line of _winningIndeces) {
-            if (_gbArray[line[0]] === _gbArray[line[1]] && _gbArray[line[0]] === _gbArray[line[2]]) {
-                return line[0]; // Return the player who won
+        // Check all of winning indeces
+        for (const line of _winningIndeces) {
+            if (_gbArray[line[0]] && _gbArray[line[0]] === _gbArray[line[1]] && _gbArray[line[0]] === _gbArray[line[2]]) {
+                _cells.forEach(cell => cell.classList.add("taken"));
+                return {end: true, winner: _gbArray[line[0]]}; // Return the player who won
             }
         }
-        return false;
+
+        // Check if it's a tie
+        if (_freeSpaces === 0) {
+            return {end: true, winner: "tie"};
+        }
+
+        return {end: false, winner: undefined};
+    }
+
+    function initializeBoard() {
+        resetBoard();
+        _cells.forEach(cell => cell.addEventListener("click", (e) => executeMove(e.currentTarget, gameController.getCurrentTurnPlayer())));
+        renderBoard();
     }
 
     function resetBoard() {
-        // Clears the board
+        // Clear the board
         _gbArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+        // Reset cell classes
+        _cells.forEach(cell => cell.classList.remove('taken'));
+
+        _freeSpaces = 9;
+
+        renderBoard();
     }
 
+    function executeMove(cell, player) {
+        if (player === undefined || gameController.getGameWon()) { return; }
 
-    function executeMove(idx, symbol) {
-        // Tries to play 'symbol' at index 'idx'. Returns true/false, and changes the board.
+        console.log(player.name + " just made a play.");
+
+        let idx = cell.dataset.idx - 0;
         if (!_gbArray[idx]) {
-            _gbArray[idx] = symbol;
-            return true;
+            _gbArray[idx] = player;
+            _freeSpaces -= 1;
+            cell.classList.add("taken");
+            gameController.changeTurn();
         } else {
-            return False
+            console.log("Taken!");
         }
+
+        gameController.updateGame();
     }
 
     function renderBoard() {
-        console.log(_gbArray);
+        _cells.forEach(cell => {
+            let idx = cell.dataset.idx - 0;
+            cell.innerHTML = _gbArray[idx] ? _gbArray[idx].symbol : "";
+        });
     }
 
-    return { resetBoard, executeMove, checkWinCondition, renderBoard }
+    return { resetBoard, executeMove, checkWinCondition, initializeBoard, renderBoard }
 })()
 
 
-// Player object
+// Player factory function
 function playerFactory(name, symbol) {
     var name = name;
     var symbol = symbol;
 
-    function makeMove() {
-        // Prompts user/comp to make their move. Returns index of tile played.
-
-    }
-
-    return { name, symbol, makeMove }
+    return { name, symbol }
 }
 
-// GameController object
+// GameController module
 var gameController = (function() {
     'use strict';
 
     var _player1, _player2;
     var _currentTurnPlayer;
+    var _gameWon = false;
+    
 
-    function _initializeGame() {
-        // Determine the players
-        _player1 = playerFactory(prompt("Player 1 name:"), "X");
-        _player2 = playerFactory(prompt("Player 2 name:"), "O");
-
-        _currentTurnPlayer = _player1;
-    }
-
-    function _changeTurn() {
+    function changeTurn() {
         if (_player1 === _currentTurnPlayer) {
             _currentTurnPlayer = _player2;
         } else {
             _currentTurnPlayer = _player1;
         }
     }
+    
+    function getCurrentTurnPlayer() {
+        return _currentTurnPlayer;
+    }
+
+    function getGameWon() {
+        return _gameWon;
+    }
 
     function startGame() {
-        _initializeGame();
+        // Determine the players
+        _player1 = playerFactory(prompt("Player 1 name:"), "X");
+        _player2 = playerFactory(prompt("Player 2 name:"), "O");
 
-        // Main game loop
-        while (true) {
-            console.log(`${_currentTurnPlayer.name}'s turn.`);
+        _currentTurnPlayer = _player1;
+        gameboard.initializeBoard();
+    }
 
-            let move = _currentTurnPlayer.makeMove();
-            if (!gameboard.executeMove(move, _currentTurnPlayer)) {
-                console.log("That spot is already taken.");
+    function updateGame() {
+        // Render the new gameboard, check for winners
+        gameboard.renderBoard();
+
+        let potentialWinner = gameboard.checkWinCondition();
+        if (potentialWinner.end) {
+            if (potentialWinner.winner !== "tie") {
+                console.log(`${potentialWinner.winner.name} won!`);
             } else {
-                _changeTurn();
+                console.log("It's a cat's game!");
             }
-
-            gameboard.renderBoard();
-
-            let potentialWinner = gameboard.checkWinCondition();
-            if (potentialWinner) {
-                console.log(`${potentialWinner.name} won!`);
-            }
-
-
+            _gameWon = true;
         }
     }
 
     function resetGame() {
         // Called when a reset button is called. 
+        _gameWon = false;
+        gameboard.resetBoard();
     }
 
 
-    return { startGame, resetGame }
+    return { startGame, updateGame, resetGame, getCurrentTurnPlayer, getGameWon, changeTurn }
 })()
+
+
+
+function start() {
+    gameController.startGame();
+
+    document.querySelector(".resetButton").addEventListener("click", gameController.resetGame);
+}
+
+window.onload = start;
